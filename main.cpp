fe2a4227ffefad7e68fa5f35de8b44220adebba8
CampingAvocado <msaladin@proton.me>
2025-03-19 17:52:11 +0100

    empty boost test framework

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/FFT>
+#include <vector>
+#include <random>
+#include <complex>
+#include <cmath>
+
+// #include "data_gen.hpp"
+ 
+// Main test suite
+BOOST_AUTO_TEST_SUITE(EigenFFTTests)
+
+
+BOOST_AUTO_TEST_SUITE_END()
